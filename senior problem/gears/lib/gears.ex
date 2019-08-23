d5faa672f91df05168e85220f7f7dd8b4197aee5
Mitchell Tenney <mitchell.tenney@vivint.com>
2019-08-22 20:25:08 -0600

    Added moduledoc to Gears

--- senior problem/gears/lib/gears.ex
+++ senior problem/gears/lib/gears.ex
@@ -1,4 +1,10 @@
 defmodule Gears do
+  @moduledoc """
+  This is the Gears Module. It is used to compute the smallest initial gear required to
+  turn 2x faster than the input gear for the given problem.
+
+  TODO: Read the prompt a couple more times to better explain the problem.
+  """
 
   def answer(pegs) do
     [-1, -1]
